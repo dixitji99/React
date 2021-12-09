fe1355235f54de85a97a94e7141b8915a9e5b6e4
Ayush <Ayush_Dixit@epam.com>
2021-12-09 11:49:58 +0530

    mod-style-css-order-clean-code

--- my-app/src/Components/Home/AboutBook.tsx
+++ my-app/src/Components/Home/AboutBook.tsx
@@ -1,37 +1,30 @@
 import { useLocation } from 'react-router-dom';
 import './aboutbook.style.scss'
 
-
 const AboutBook = () => {
     const location = useLocation();
     return (
-        <div className="book-details">
-            <div className="container">
-                <div className="row mt-3">
-                    <div className="col-xs-12 col-sm-12 col-md-4 col-lg-3">
-                        <div className="card">
-                            <img src={location.state._image} alt="image1" />
-                        </div>
-                    </div>
-                    <div className="col-xs-12 col-sm-12 col-mg-9 col-lg-9 d-f c-c">
-                        <h3 >{location.state._name}</h3>
-                        <h6>${location.state._price}</h6>
-                        <h6>{location.state._author}</h6>
-                        <h6>Page Count:{location.state._page}</h6>
-                        <h6>Book ISBN: {location.state._ISBN}</h6>
-                        <div className="mt-3">
-                            <button className="btn btn-primary mx-2">Add to Cart</button>
-                        <button className="btn btn-primary mx-2">Buy Now</button>
-                    </div>
-                    <div className="mt-3 mx-2">
-                        <textarea name="description" id="bookDescription" className="px-3" disabled>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates provident nam distinctio harum? Atque voluptates impedit quasi rerum, doloribus, asperiores iusto fugiat assumenda ut inventore voluptatibus quae quidem repellat perferendis!
-                        </textarea>
-                    </div>
+        <div className="about-book">
+            <div className="about-book__image">
+                <img src={location.state._image} alt="image1" className="about-book__image--cover-image" />
+            </div>
+            <div className="about-book__information">
+                <span className="about-book__information--title">{location.state._name}</span>
+                <span className="about-book__information--price">${location.state._price}</span>
+                <span className="about-book__information--author">{location.state._author}</span>
+                <span className="about-book__information--pages"><strong style={{color:"black"}}>Page Count: </strong>{location.state._page}</span>
+                <span className="about-book__information--isbn"><strong style={{color:"black"}}>Book ISBN: </strong>{location.state._ISBN}</span>
+                <div className="about-book__cart-buttons">
+                    <button className="about-book__cart-buttons--btn">Add to Cart</button>
+                    <button className="about-book__cart-buttons--btn">Buy Now</button>
+                </div>
+                <div className="">
+                    <textarea name="description" id="bookDescription" className="" disabled>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates provident nam distinctio harum? Atque voluptates impedit quasi rerum, doloribus, asperiores iusto fugiat assumenda ut inventore voluptatibus quae quidem repellat perferendis!
+                    </textarea>
                 </div>
             </div>
-        </div>            
-        </div >
+        </div>
     )
 }
 
